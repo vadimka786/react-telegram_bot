468d1eb3e18bd3ba44943922472b30ef2b31e277
vadimka786 <prasolovvadim6@gmail.com>
2022-11-08 01:01:50 +1000

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
@@ -8,14 +8,10 @@ function App() {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
 
   return (
     <div className="App">
-      Ok
-      <button onClick={ onClose }>X</button>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close();
+    }
+
+    return (
+        <div className={'header'}>
+            <Button onClic={onClose} >Закрыть</Button>
+            <span className={'username'} >
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
